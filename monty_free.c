5937e80ebe178b9aefc82202cc8cf8017c90d202
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-11-17 10:44:41 +0200

    Initial commit

---
+++ monty_free.c
@@ -0,0 +1,23 @@
+/*
+ * File name: monty_free.c
+ * Authors: Ricardo Sass
+ */
+
+#include "monty.h"
+
+/**
+ * free_stack - A function that frees the memory allocated for stack.
+ * @stack: Pointer to the doubly linked list representation of a stack.
+ * Return: Nothing.
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *tmp = *stack;
+
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
